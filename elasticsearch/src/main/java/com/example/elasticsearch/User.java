9d2eef07c9f803d22fe624e07358a7cc7c16aa53
qiuhonglong <qiuhonglong@chinatelecom.cn>
2020-10-23 10:55:46 +0800

    添加elasticsearch拼音搜索示例

---
+++ elasticsearch/src/main/java/com/example/elasticsearch/User.java
@@ -0,0 +1,10 @@
+package com.example.elasticsearch;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String nickName;
+
+}
--- elasticsearch/src/test/java/com/example/elasticsearch/ElasticsearchApplicationTests.java
+++ elasticsearch/src/test/java/com/example/elasticsearch/ElasticsearchApplicationTests.java
@@ -1,14 +1,19 @@
 package com.example.elasticsearch;
 
+import org.apache.commons.lang3.StringUtils;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.MultiMatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.rescore.QueryRescorerBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class ElasticsearchApplicationTests {
@@ -69,19 +74,43 @@ class ElasticsearchApplicationTests {
 //        EsUtil.PageResponse<Blog> pageResponse = esUtil.search("demo1_blog", searchSourceBuilder, Blog.class, 10, 0);
 
 //--
+//        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+//
+//        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery()
+//                .filter(QueryBuilders.multiMatchQuery("系统学习ElasticSearch", "title", "content", "tag"))
+//                .filter(QueryBuilders.termQuery("author", "方才兄"))
+//                .filter(QueryBuilders.termsQuery("tag.keyword", "ElasticSearch", "倒排序索引"))
+//                .filter(QueryBuilders.rangeQuery("influence").gte(10).lte(15));
+//
+//        searchSourceBuilder.query(boolQuery);
+//        searchSourceBuilder.sort("view", SortOrder.DESC);
+//
+//        EsUtil.PageResponse<Blog> pageResponse = esUtil.search("demo1_blog", searchSourceBuilder, Blog.class, 10, 0);
+
+
+//--
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
 
-        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery()
-                .filter(QueryBuilders.multiMatchQuery("系统学习ElasticSearch", "title", "content", "tag"))
-                .filter(QueryBuilders.termQuery("author", "方才兄"))
-                .filter(QueryBuilders.termsQuery("tag.keyword", "ElasticSearch", "倒排序索引"))
-                .filter(QueryBuilders.rangeQuery("influence").gte(10).lte(15));
+        String query = "关zhuwo";
+        Pattern pattern = Pattern.compile("[^\u4E00-\u9FA5]");
+        Matcher matcher = pattern.matcher(query);
+        String chinese = matcher.replaceAll("");
+        System.out.println(chinese);
 
-        searchSourceBuilder.query(boolQuery);
-        searchSourceBuilder.sort("view", SortOrder.DESC);
+        // 必须包含中文
+        if (StringUtils.isNotBlank(chinese))
+            boolQuery.filter(QueryBuilders.matchQuery("nickName", chinese).analyzer("standard"));
 
-        EsUtil.PageResponse<Blog> pageResponse = esUtil.search("demo1_blog", searchSourceBuilder, Blog.class, 10, 0);
+        // 中文 + 全拼 + 首字母 混合检索
+        boolQuery.must(QueryBuilders.matchPhraseQuery("nickName.pinyin", query));
+        // 高亮设置
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("nickName.pinyin");
 
+        searchSourceBuilder.query(boolQuery).highlighter(highlightBuilder).size(10).from(0);
+
+        EsUtil.PageResponse<User> response = esUtil.search("user_index", searchSourceBuilder, User.class, 0, 10);
 
         System.out.println("------");
     }
