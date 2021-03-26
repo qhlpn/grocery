23809ed5d165d3abccaad89a038901f3e55d9fed
qhlpn <742686556@qq.com>
2021-03-26 17:37:51 +0800

    modify

--- javase/src/com/example/io/IOoperate.java
+++ javase/src/com/example/io/IOoperate.java
@@ -11,22 +11,29 @@ public class IOoperate {
     // 字节流：InputStream / OutputStream
     // 字符流：Reader / Writer
 
+    // 装饰者模式
+
 
     public static void main(String[] args) throws IOException {
+
 //        file();
-        inputOutputStream();
+//        inputOutputStream();
+//        objectSerialStream();
+//        readerWriter();
+//        inputStreamToReaderAndOutputStreamToWriter();
+
     }
 
 
     private static void file() throws IOException {
 
-        String var1 = "C:\\Users\\74268\\Desktop\\demo.txt";
+        String var1 = ".\\demo.txt";
         File file = new File(var1);
         System.out.println(file.createNewFile());
         System.out.println(file.isFile());
         System.out.println(file.delete());
 
-        String var2 = "C:\\Users\\74268\\Desktop";
+        String var2 = ".\\src";
         File dire = new File(var2);
         System.out.println(dire.isDirectory());
         File[] files = dire.listFiles((dir, name) -> true);
@@ -40,8 +47,11 @@ public class IOoperate {
     private static void inputOutputStream() throws IOException {
 
         // abstract -> InputStream  OutputStream
+        // impl -> FileInputStream  FileOutputStream
 
-        String path = "C:\\Users\\74268\\Desktop\\demo.txt";
+        String path = ".\\demo.txt";
+        File file = new File(path);
+        file.createNewFile();
 
         // impl -> FileOutputStream
         // append -> true / false
@@ -50,7 +60,6 @@ public class IOoperate {
             byte[] buffer = new byte[]{10, 13, 72, 101, 108, 108, 111};
             os.write(buffer);
         }
-
 
         // impl -> FileInputStream
         try (InputStream is = new FileInputStream(path)) {
@@ -65,6 +74,120 @@ public class IOoperate {
             System.out.println(sb.toString());
         }
 
+        file.delete();
+
     }
+
+
+    /**
+     * Java序列化机制将对象变为byte[]数组
+     */
+    public static class SerialObject implements Serializable {
+
+        // 非必需字段，自动阻止不匹配的版本，以防抛出 InvalidClassException
+        private static final long serialVersionUID = 1L;
+
+        private int id = 10;
+
+        // 反序列化时不会执行构造函数，因为是序列化对象（属性值），而不是类
+        public SerialObject() {
+            id = 10;
+            System.out.println("~~~~~~~~~~~~~~~~~~");
+        }
+
+        public int getId() {
+            return id;
+        }
+
+    }
+
+    private static void objectSerialStream() throws IOException {
+
+        // abstract -> InputStream  OutputStream
+        // impl -> ObjectInputStream  ObjectOutputStream
+
+        String path = ".\\demo.txt";
+        File file = new File(path);
+        file.createNewFile();
+
+        SerialObject o1 = new SerialObject();
+        System.out.println(o1.getId());
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+            oos.writeObject(o1);
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+            Object o = ois.readObject();
+            if (o instanceof SerialObject) {
+                SerialObject o2 = (SerialObject) o;
+                System.out.println(o2.getId());
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        file.delete();
+
+    }
+
+    private static void readerWriter() throws IOException {
+
+        // abstract -> Reader  Writer
+        // impl -> FileReader FileWriter
+
+        String path = ".\\demo.txt";
+        File file = new File(path);
+        file.createNewFile();
+
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write("hello word !!");
+        }
+
+        try (FileReader fr = new FileReader(file)) {
+            char[] buffer = new char[1024];
+            int number;
+            while ((number = fr.read(buffer)) != -1) {
+                System.out.println(String.valueOf(buffer, 0, number));
+            }
+        }
+        file.deleteOnExit();
+    }
+
+
+    /**
+     * 装饰模式
+     * @throws IOException
+     */
+    private static void inputStreamToReaderAndOutputStreamToWriter() throws IOException {
+
+        // Reader 实际上是基于 InputStream 构造
+        // InputStreamReader 作为转换器，可将任何的 InputStream Impl 转换为 Reader Abstract
+
+        // Writer 与 OutputStream 间关系同理
+
+        String path = ".\\demo.txt";
+        File file = new File(path);
+        file.createNewFile();
+
+        try (FileOutputStream fos = new FileOutputStream(file)) {
+            try (Writer w = new OutputStreamWriter(fos)) {
+                w.write("hello world !!!");
+            }
+        }
+
+        try (FileInputStream fis = new FileInputStream(file)){
+            try (Reader r = new InputStreamReader(fis)) {
+                char[] buffer = new char[1024];
+                int number;
+                while ((number = r.read(buffer)) != -1) {
+                    System.out.println(String.valueOf(buffer, 0, number));
+                }
+            }
+
+        }
+
+        file.delete();
+    }
+
 
 }
