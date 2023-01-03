cd753de844ff9a6c1e7af81b6392155a690cc2b5
qiuhonglong <qiuhonglong@chinatelecom.cn>
2023-01-03 18:34:38 +0800

    modify

--- golang/go.mod
+++ golang/go.mod
@@ -19,14 +19,18 @@ require (
 	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/btree v1.0.1 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
--- golang/pkg/client_go.go
+++ golang/pkg/client_go.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/discovery/cached/disk"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"os"
+	"time"
 )
 
 func ClientGoFunc() {
@@ -20,7 +24,12 @@ func ClientGoFunc() {
 	//RESTClient()
 	//ClientSet()
 	//DynamicClient()
-	DiscoveryClient()
+	//DiscoveryClient()
+	//GVRCache()
+	//ListWatch()
+	//ShareInformer()
+	SampleController()
+
 }
 
 var config = &rest.Config{
@@ -165,3 +174,110 @@ func DiscoveryClient() {
 		}
 	}
 }
+
+/**
+GVR数据其实很少变动，因此我们可以缓存GVR数据，来减少Client与APIServer交互
+缓存到本地：CachedDiscoveryClient
+缓存到内存：memCacheClient
+在运行K8s机器上，缓存信息默认存储于/.kube/cache和/.kube/http-cache
+*/
+func GVRCache() {
+
+	// 实例化客户端，本客户端负责将GVR数据缓存到本地文件中
+	cachedDiscoveryClient, err := disk.NewCachedDiscoveryClientForConfig(config, "./cache/discovery", "./cache/http", time.Minute*60)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	/*
+		1) 先从缓存文件中找GVR数据，有则直接返回，没有则调用APIServer
+		2) 调用APIServer获取GVR数据
+		3) 将获取到的GVR数据缓存到本地，然后返回给客户端
+	*/
+	_, apiResourcesList, err := cachedDiscoveryClient.ServerGroupsAndResources()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, list := range apiResourcesList {
+		gv, err := schema.ParseGroupVersion(list.GroupVersion)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		for _, resource := range list.APIResources {
+			fmt.Printf("name: %v, group: %v, version: %v\n", resource.Name, gv.Group, gv.Version)
+		}
+	}
+}
+
+/**
+List-Watch它分为两个部分：
+list负责调用资源对应的Kubernetes APIServer的restful API获取全局数据列表，并同步到本地缓存中。
+watch负责监听资源变化，并调用相应的事件处理函数进行处理，同时更新本地缓存，使本地缓存与Etcd中数据保持一致。
+list是基于HTTP中的短链接实现。watch则是基于HTTP长链接实现
+*/
+func ListWatch() {
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 调用监听的方法
+	watch, err := clientset.AppsV1().Deployments("kube-system").Watch(context.TODO(), metav1.ListOptions{}) //返回一个通道
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		select {
+		//多路复用
+		case e, _ := <-watch.ResultChan(): //监听通道，实时拿到最新消息
+			fmt.Println(e.Type, e.Object)
+			//e.Type表示事件变化的类型， Added ， Delete等
+			//e.Object 表示变化后的数据
+		}
+	}
+}
+
+func ShareInformer() {
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 初始化 sharedInformerFactory
+	sharedInformerFactory := informers.NewSharedInformerFactory(clientset, 0)
+
+	// 生成 podInformer
+	podInformer := sharedInformerFactory.Core().V1().Pods()
+
+	// 生成 indexer 用于查询数据
+	indexer := podInformer.Lister()
+
+	// 启动 sharedInformerFactory
+	sharedInformerFactory.Start(nil)
+
+	// 等待数据同步完成
+	sharedInformerFactory.WaitForCacheSync(nil)
+
+	// 利用 indexer 获取数据
+	pods, err := indexer.List(labels.Everything())
+	if err != nil {
+		panic(err.Error())
+	}
+	for _, pod := range pods {
+		fmt.Printf("namespace: %v, name: %v\n", pod.Namespace, pod.Name)
+	}
+}
+
+func SampleController() {
+	// https://github.com/kubernetes/sample-controller
+	// + https://andblog.cn/3196
+	// + https://blog.51cto.com/daixuan/5175780
+}
+
+————————————————
+版权声明：本文为CSDN博主「小博士不知」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/javaldk/article/details/122590998
